b622f989596894256f51e85f7f5e2b356a3482fe
erikathea <ethajes@gmail.com>
2014-08-28 16:48:26 +0800

    v2: hash implementation

--- usernameGenerator.rb
+++ usernameGenerator.rb
@@ -4,20 +4,39 @@ class UsernameGenerator
 
 	# names 		- cache, list of usernames
 	# db_count 	- flag, number of usernames
-	attr_accessor :names, :db_count
+	attr_accessor :names, :db_count, :hashed_names
 
-	def initialize
+	def initialize(version=1)
 		# it may take a while, depends on the
 		# size of file: usernames.txt
-		loadDatabase
+		if version > 1
+			loadHashDatabase
+		else
+			loadArrayDatabase
+		end
 	end
 
 	def generate(email)
-		# extract username from email address given
-		# e.g. paul-angelo@mail.com to paulangelo
-		username = email.split('@').first.gsub(/-|_/, '')
-		listed_names = @names.select { |name| name.include? username }
+		if @names
+			generate_via_array(email)
+		elsif @hashed_names
+			generate_via_hash(email)
+		end
+	end
 
+	def refresh
+		if @names
+			updateArrayDatabase
+		elsif @hashed_names
+			updateHashDatabase
+		end
+	end
+
+	private
+
+	def generate_via_array(email)
+		username = email.split('@').first.gsub(/-|_/, '')
+		listed_names =	@names.select { |name| name.include? username }
 		# appends number if username is taken
 		# e.g. paulangelo to paulangelo1
 		username = "#{username}#{@names.size}" unless listed_names.empty?
@@ -27,13 +46,20 @@ class UsernameGenerator
 		return username
 	end
 
-	def refresh
-		updateDatabase
+	def generate_via_hash(email)
+		key = email.split('@').first.gsub(/-|_/, '').split(/\d*\z/)
+
+		if @hashed_names.has_key?(key)
+			@hashed_names[key] = "#{(@hashed_names[key]).to_i + 1}"
+			username = "#{key.first}#{@hashed_names[key]}"
+		else
+			@hashed_names[key] = 0
+		end
+
+		return username
 	end
 
-	private
-
-	def loadDatabase
+	def loadArrayDatabase
 		# usernames.txt serves as db storage
 		file = File.open('usernames.txt', 'r')
 		@names = ((file.read).split("\n"))
@@ -41,17 +67,48 @@ class UsernameGenerator
 		file.close
 	end
 
-	def updateDatabase
+	def loadHashDatabase
+		names = {}
+		file = File.open('usernames.txt', 'r')
+		((file.read).split("\n")).each do |name|
+			name = name.strip
+			unless name.empty?
+				key = name.split(/\d*\z/)
+				value = name.match(/\d*\z/)[0] # MatchData
+				names[key] = value
+				#puts value if key == "john"
+				#if names.has_key?(key)
+				#	names[key] << value
+				#else
+				#	names[key] = [value]
+				#end
+
+			end
+		end
+
+		@hashed_names = names
+		@db_count = @hashed_names.size
+	end
+
+	def updateArrayDatabase
 		file = File.open('usernames.txt', 'a+')
 
-		# add new usernames to usernames.txt file
-		# start_index = @db_count -1
-		# end_index = @names.size
 		@names[(@db_count - 1)...@names.size].each do |name|
 			file.write("\n#{name}")
 		end
 
 		file.close
 		@db_count = @names.size
+	end
+
+	def updateHashDatabase
+		file = File.open('usernames.txt', 'a+')
+
+		((@db_count + 1)..@hashed_names.size).each do |index|
+			file.write("\n#{@hashed_names[index]}")
+		end
+
+		file.close
+		@db_count = @hashed_names.size
 	end
 end
